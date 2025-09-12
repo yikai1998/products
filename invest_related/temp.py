8011756a90d9c30d88a90c777a95adb30568efd0
yikai <c1998828@icloud.com>
2025-09-12 12:33:34 +0800

    Update temp.py

--- invest_related/temp.py
+++ invest_related/temp.py
@@ -57,6 +57,7 @@ def fetch_all_nav(fund_code: str):
         .sort_values("date")
         .reset_index(drop=True)
     )"""
+    df["基金代码"] = fund_code
     df["净值日期"] = pd.to_datetime(df["净值日期"])
     df["MA20"] = df["单位净值"].rolling(20).mean()
     df["MA120"] = df["单位净值"].rolling(120).mean()
@@ -66,39 +67,61 @@ def fetch_all_nav(fund_code: str):
         pct = (history > df["单位净值"].iloc[i]).mean()
         pct_list_all.append(pct)
     df["低于历史价值百分比"] = pct_list_all
+    # 动态阈值：随过去 250 天滚动，从低到高排(从高估到低估)，低于历史价值百分比的第35%｜70%分位值
+    df["高估边界"] = df["低于历史价值百分比"].rolling(360).quantile(0.02)
+    df["低估边界"] = df["低于历史价值百分比"].rolling(360).quantile(0.8)
     pct_list_60 = [None] * 60
     for i in range(60, len(df)):
         history = df["单位净值"].iloc[(i-60):i]
         pct = (history > df["单位净值"].iloc[i]).mean()
         pct_list_60.append(pct)
     df["低于过去60日的价值百分比"] = pct_list_60
-    df["基金代码"] = fund_code
+    df["低于过去60日的价值百分比"] = df["低于过去60日的价值百分比"]
     df["日增长率"] = round(df["日增长率"] / 100, 4)
     df["日增长率滑动均值"] = df["日增长率"].rolling(10).mean()
-    df["近15日高点"] = df["单位净值"].rolling(15).max()
-    df["距离高点回撤比"] = (df["单位净值"] - df["近15日高点"]) / df["近15日高点"]
+    df["日增长率滑动均值"] = df["日增长率滑动均值"]
+    df["季度最大回撤"] = (df["单位净值"] - df["单位净值"].rolling(30).max()) / df["单位净值"].rolling(90).max()
+    df["年最大回撤"] = (df["单位净值"] - df["单位净值"].rolling(360).max()) / df["单位净值"].rolling(360).max()
+    df["连跌恐慌"] = (df["日增长率"] < -0.01).rolling(5).sum() >= 3
     tag = []
+
     for i in range(len(df)):
         price = df.loc[i, "单位净值"]
         ma20 = df.loc[i, "MA20"]
         ma120 = df.loc[i, "MA120"]
-        p_total = df.loc[i, "低于历史价值百分比"]
-        p_recent = df.loc[i, "低于过去60日的价值百分比"]
-        mean_growth = df.loc[i, "日增长率滑动均值"]
-        drawdown = df.loc[i, "距离高点回撤比"]
-        if (price > ma20) and (price > ma120) and (ma20 > ma120) and (mean_growth < 0.001) and (drawdown < -0.05):
-            tag.append("上涨势头可能要结束了")
-        elif (price > ma20) and (price > ma120) and (ma20 > ma120) and (p_total >= 0.7) and (p_recent >= 0.85):
+        p_under_total = df.loc[i, "低于历史价值百分比"]
+        p_under_60d = df.loc[i, "低于过去60日的价值百分比"]
+        mean_growth_10d = df.loc[i, "日增长率滑动均值"]
+        qdraw = df.loc[i, "季度最大回撤"]
+        hydraw = df.loc[i, "年最大回撤"]
+        p_under_360d_high, p_under_360d_low = df.loc[i, "高估边界"],  df.loc[i, "低估边界"]
+        panic = df.loc[i, "连跌恐慌"]
+
+        if (price > ma20) and (ma20 > ma120) and (mean_growth_10d > 0.001) and (qdraw >= -0.05) and (p_under_total > p_under_360d_high):
+            tag.append("良性上涨, 可适量增持")
+        elif (price > ma20) and (price > ma120) and (p_under_total <= p_under_360d_high):
+            tag.append("高估, 不宜加仓")
+        elif (price < ma20) and (price < ma120) and (p_under_total <= p_under_360d_high):
+            tag.append("动力不强, 但仍高估, 观望/减仓")
+        elif (price > ma20) and (price > ma120) and (ma20 > ma120) and (mean_growth_10d < 0.001) and (qdraw < -0.05):
+            tag.append("上涨尾声, 分批落袋")
+        elif (price < ma20) and (price < ma120) and (p_under_total >= p_under_360d_low):
+            tag.append("动力不强, 但低估, 可考虑分批")
+        elif (price > ma120 or p_under_total >= p_under_360d_low) and (hydraw < -0.2) and (p_under_60d >= 0.85):
             tag.append("低估, 可买")
-        elif (price > ma120) and (p_total >= 0.7):
-            tag.append("关注, 可能还能跌")
-        elif (price < ma20) and (price < ma120) and (p_total <= 0.25):
-            tag.append("高估, 别持有")
+        elif panic and (p_under_total >= p_under_360d_low):
+            tag.append("连跌加速, 可关注/耐心等待")
+
         else:
             tag.append("合理区间波动")
+
     df["信号"] = tag
     df["低于过去60日的价值百分比"] = df["低于过去60日的价值百分比"].map(lambda x: '%.5f' % x)
     df["日增长率滑动均值"] = df["日增长率滑动均值"].map(lambda x: '%.5f' % x)
+
+    df['信号标记'] = (df['信号'] != df['信号'].shift()).cumsum()  # 列整体向下移动一行, 判断变化, 标记同一信号连续出现的段落
+    df['信号连续天数'] = df.groupby('信号标记').cumcount() + 1  # 统计同一段的第几天
+
     df.sort_values("净值日期", inplace=True)
     df.reset_index(drop=True, inplace=True)
 
@@ -154,8 +177,8 @@ def manual_parse(symbol: str, year: str):
              soup.find_all("h4", class_="t")]  # <h4 class="t">2024Q1</h4>“分隔符”, \xa0\xa0“不间断空格”
 
     big = pd.DataFrame()
-    for q, html_table in enumerate(pd.read_html(io.StringIO(data["content"]), converters={
-        "股票代码": str})):  # 把字符串“伪装”成类文件对象，让 pandas 走“内存文本”分支，一次性读表。强制将股票代码当作str看待
+    for q, html_table in enumerate(pd.read_html(io.StringIO(data["content"]), converters={"股票代码": str})):
+        # 把字符串“伪装”成类文件对象，让 pandas 走“内存文本”分支，一次性读表。强制将股票代码当作str看待
         # 把所有列名里的空格、%、全角括号全干掉，方便后面模糊匹配
         html_table.columns = [re.sub(r"[ %　\(\)（）]", "", c.strip()) for c in html_table.columns]
         ratio_col = next((c for c in html_table.columns if "占净值" in c), None)  # “找第一个满足条件的元素”的惯用写法，找不到就 None
